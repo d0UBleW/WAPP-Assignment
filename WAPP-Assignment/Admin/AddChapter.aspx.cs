464345a7fdefa2d12dc8b1d3b5a8fb1ce41865fe
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2022-04-30 23:07:42 +0800

    feat(chapter): add chapter from edit interface

--- WAPP-Assignment/Admin/AddChapter.aspx.cs
+++ WAPP-Assignment/Admin/AddChapter.aspx.cs
@@ -13,10 +13,12 @@ namespace WAPP_Assignment.Admin
 {
     public partial class AddChapter : System.Web.UI.Page
     {
-        private SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["iLearnDBConStr"].ConnectionString);
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+            }
         }
 
         protected void AddBtnASP_Click(object sender, EventArgs e)
@@ -31,15 +33,21 @@ namespace WAPP_Assignment.Admin
             }
             string title = TitleTxtBox.Text;
             string content = dataField.Value;
-            conn.Open();
-            SqlCommand cmd = new SqlCommand();
-            cmd.CommandText = "INSERT INTO chapter (course_id, title, content) VALUES (@course_id, @title, @content);";
-            cmd.Connection = conn;
-            cmd.Parameters.AddWithValue("@course_id", course_id);
-            cmd.Parameters.AddWithValue("@title", title);
-            cmd.Parameters.AddWithValue("@content", content);
-            cmd.ExecuteNonQuery();
-            conn.Close();
+            using (SqlConnection conn = DatabaseManager.CreateConnection())
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.CommandText = "INSERT INTO chapter (course_id, title, content) VALUES (@course_id, @title, @content);";
+                    cmd.Connection = conn;
+                    cmd.Parameters.AddWithValue("@course_id", course_id);
+                    cmd.Parameters.AddWithValue("@title", title);
+                    cmd.Parameters.AddWithValue("@content", content);
+                    cmd.ExecuteNonQuery();
+                }
+                conn.Close();
+            }
+            Response.Redirect($"/Admin/EditCourse.aspx?course_id={course_id}");
         }
     }
 }
---
+++ WAPP-Assignment/Admin/EditCourse.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="EditCourse.aspx.cs" Inherits="WAPP_Assignment.Admin.EditCourse" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="TitleLbl" runat="server" Text="Title"></asp:Label>
+            <asp:TextBox ID="TitleTxtBox" runat="server"></asp:TextBox>
+            <br /><br />
+            <asp:Label ID="DescLbl" runat="server" Text="Description"></asp:Label>
+            <asp:TextBox ID="DescTxtBox" runat="server"></asp:TextBox>
+            <br /><br />
+            <asp:Button ID="AddChapBtn" runat="server" Text="Add Chapter" OnClick="AddChapBtn_Click" />
+            <br /><br />
+            <asp:Placeholder ID="ChapterPlaceholder" runat="server"></asp:Placeholder>
+            <asp:RadioButtonList ID="RadioButtonList1" runat="server" AutoPostBack="true"></asp:RadioButtonList>
+        </div>
+    </form>
+</body>
+</html>
