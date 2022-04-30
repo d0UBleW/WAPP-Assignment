3b14e6ef5293817433c44ce1a5fc7142158e1383
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2022-04-30 22:37:55 +0800

    feat(course): list course

---
+++ WAPP-Assignment/Scripts/addChapter.js
@@ -0,0 +1,20 @@
+﻿const updateData = (ed) => {
+    $("#dataField").val(ed.getData())
+}
+
+DecoupledEditor
+    .create(document.querySelector('#editor'), {
+        mediaEmbed: {
+            previewsInData: true
+        },
+    } )
+    .then( editor => {
+        const toolbarContainer = document.querySelector( '#toolbar-container' );
+        toolbarContainer.appendChild( editor.ui.view.toolbar.element );
+        editor.model.document.on('change:data', (evt, data) => {
+            updateData(editor)
+        })
+    } )
+    .catch( error => {
+        console.error( error );
+    } );
---
+++ WAPP-Assignment/ViewCourse.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ViewCourse.aspx.cs" Inherits="WAPP_Assignment.ViewCourse" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:PlaceHolder ID="CoursePlaceholder" runat="server"></asp:PlaceHolder>
+            <asp:RadioButtonList ID="RadioButtonList1" runat="server" AutoPostBack="True"></asp:RadioButtonList>
+        </div>
+    </form>
+</body>
+</html>
