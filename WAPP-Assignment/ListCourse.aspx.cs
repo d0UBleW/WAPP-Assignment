d6e5f0f2ca35417bc6c379dab72daf758441929f
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2022-05-05 12:30:22 +0800

    button based on user type

--- WAPP-Assignment/ListCourse.aspx.cs
+++ WAPP-Assignment/ListCourse.aspx.cs
@@ -16,49 +16,82 @@ namespace WAPP_Assignment
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            //if (IsPostBack)
-            //{
-            //    if (Request.Form["__EVENTTARGET"] != null && Request.Form["__EVENTTARGET"] == "EditCourseBtn")
-            //    {
-            //        EditCourseBtn_Click(null, null);
-            //    }
-            //}
             DataTable dt = Course.GetAllCourseData();
-            StringBuilder courseContainer = new StringBuilder();
             foreach (DataRow dr in dt.Rows)
             {
-                courseContainer.AppendLine("<div class=\"container\">");
-                courseContainer.AppendLine("<div class=\"image-container\">");
-                courseContainer.AppendLine($"<img src=\"/upload/loading.gif\" onload=\"this.onload=null;this.src='/upload/thumbnail/{dr["thumbnail"]}'\" width=200px, height=200px />");
-                courseContainer.AppendLine("</div>");
-                courseContainer.AppendLine($"<h3>{dr["title"]}</h3>");
-                courseContainer.AppendLine($"<span>{dr["description"]}</span>");
-                courseContainer.AppendLine("</div>");
-                courseContainer.AppendLine("<br />");
-                // courseContainer.AppendLine($"<input type=\"button\" value=\"Edit Course\" onclick='javascript:__doPostBack(\"EditCourseBtn\", \"{dr["course_id"]}\")'/>");
-                CoursePlaceholder.Controls.Add(new Literal { Text = courseContainer.ToString() });
-                Button editBtn = new Button
+                Panel cPanel = new Panel();
+                Panel imgPanel = new Panel();
+                cPanel.CssClass = "container bg-blue";
+                Image thumbnail = new Image
                 {
-                    Text = "Edit Course",
-                    ID = $"editBtn{dr["course_id"]}"
+                    ImageUrl = "/upload/loading.gif",
+                    Width = 200,
+                    Height = 200,
                 };
-                editBtn.Click += new EventHandler(EditCourseBtn_Click);
-                //editBtn.Click += (s, evt) =>
-                //{
-                //    Regex rg = new Regex(@"editBtn(\d+)");
-                //    Match match = rg.Match(editBtn.ID);
-                //    string course_id = match.Groups[1].Value;
-                //    Response.Redirect($"/Admin/EditCourse.aspx?course_id={course_id}");
-                //};
-                CoursePlaceholder.Controls.Add(editBtn);
-                courseContainer.Clear();
+                cPanel.Controls.Add(imgPanel);
+                thumbnail.Attributes.Add("onload", $"javascript:this.onload=null;this.src='/upload/thumbnail/{dr["thumbnail"]}'");
+                imgPanel.Controls.Add(thumbnail);
+                HyperLink title = new HyperLink
+                {
+                    NavigateUrl = $"/ViewCourse.aspx?course_id={dr["course_id"]}",
+                    Text = dr["title"].ToString(),
+                };
+                title.Style.Add("font-size", "36px");
+                cPanel.Controls.Add(title);
+                Label description = new Label
+                {
+                    Text = dr["description"].ToString(),
+                };
+                cPanel.Controls.Add(new Literal { Text = "<br />" } );
+                cPanel.Controls.Add(description);
+                CoursePlaceholder.Controls.Add(cPanel);
+                if (Session["isAdmin"] == null)
+                {
+                }
+                else if ((bool)Session["isAdmin"])
+                {
+                    Button editBtn = new Button
+                    {
+                        Text = "Edit Course",
+                        ID = $"editBtn_{dr["course_id"]}"
+                    };
+                    editBtn.Attributes.Add("data-course-id", dr["course_id"].ToString());
+                    editBtn.Click += new EventHandler(EditCourseBtn_Click);
+                    //editBtn.Click += (s, evt) =>
+                    //{
+                    //    Regex rg = new Regex(@"editBtn(\d+)");
+                    //    Match match = rg.Match(editBtn.ID);
+                    //    string course_id = match.Groups[1].Value;
+                    //    Response.Redirect($"/Admin/EditCourse.aspx?course_id={course_id}");
+                    //};
+                    CoursePlaceholder.Controls.Add(editBtn);
+                }
+                else if (!(bool)Session["isAdmin"])
+                {
+                    Button enrollBtn = new Button
+                    {
+                        Text = "Enroll",
+                        ID = $"enrollBtn_{dr["course_id"]}"
+                    };
+                    enrollBtn.Attributes.Add("data-course-id", dr["course_id"].ToString());
+                    enrollBtn.Click += new EventHandler(EnrollBtn_Click);
+                    CoursePlaceholder.Controls.Add(enrollBtn);
+                }
+                CoursePlaceholder.Controls.Add(new Literal { Text = "<br />" });
             }
+        }
+
+        protected void EnrollBtn_Click(object sender ,EventArgs e)
+        {
+            Button btn = sender as Button;
+            string course_id = btn.Attributes["data-course-id"];
+            Response.Redirect($"/EnrollCourse.aspx?course_id={course_id}");
         }
 
         protected void EditCourseBtn_Click(object sender, EventArgs e)
         {
             Button btn = sender as Button;
-            Regex rg = new Regex(@"editBtn(\d+)");
+            Regex rg = new Regex(@"editBtn_(\d+)");
             Match match = rg.Match(btn.ID);
             string course_id = match.Groups[1].Value;
             Response.Redirect($"/Admin/Course/EditCourse.aspx?course_id={course_id}");
